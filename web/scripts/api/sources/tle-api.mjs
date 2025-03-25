b1a152c33d1dc6f9ad7c2ffff6f16948faae0214
lucas maillet <mailletl@etu.univ-grenoble-alpes.fr>
2025-03-25 19:29:22 +0100

    Add some field

--- web/scripts/api/sources/tle-api.mjs
+++ web/scripts/api/sources/tle-api.mjs
@@ -14,6 +14,30 @@ export const TleApi = new API({
 				placeholder: "e.g. ISS",
 			},
 		},
+		{ // For future functionalities
+			label: "Sort by",
+			field: {
+				name: "sort",
+				values: [
+					{ value: 'id', label: 'Identifier' },
+					{ value: 'name', label: 'Name' },
+					{ value: 'popularity', label: 'Popularity' },
+					{ value: 'inclination', label: 'Inclination' },
+					{ value: 'eccentricity', label: 'Eccentricity' },
+					{ value: 'period', label: 'Period' },
+				]
+			},
+		},
+		{
+			label: "Sort direction",
+			field: {
+				name: "sort-dir",
+				values: [
+					{ value: 'asc', label: 'Ascending' },
+					{ value: 'desc', label: 'Descending' },
+				]
+			},
+		},
 	],
 	load: async (res) =>
 		(await res.json()).member.map(({ line2, name }) => {
